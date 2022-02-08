2547dce07ae8d38aaad56b1a5d11ba4c3d8c9d71
TravisBubb <travisbubb14@gmail.com>
2022-02-08 15:27:16 -0500

    Add original version of program that was submitted for class

---
+++ src/board.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+# Author:           Travis Bubb
+# Description:      The board class for Dots and Boxes
+# Professor:        Dr. Schwesinger
+# File:             board.py
+# Date:             December 15, 2021
+
+class Board:
+    """Class to represent the Dots and Boxes game board
+    
+    Attributes:
+        size (int): The size of the board
+        state (dict): The game state that the board uses for display purposes
+    
+    """
+    
+    def __init__(self, size=4):
+        """Initialize a new board object; Initialize the board's attributes
+        
+        Parameters:
+            size (int): The desired size of the board - default=4
+        
+        """
+    
+        self.size = size
+        self.state = {}
+
+    def __str__(self):
+        """Compute and return a string representation of the board object
+        
+        Return:
+            res (str): A string representing the board; Used to display the game board
+        
+        """
+    
+        res = ''
+        
+        # Combine both players' moves into one set
+        lines = set(self.state[1]) | set(self.state[2])
+         
+        # Add the top x-axis labels to the result string
+        res += '{:2}'.format('')    # Add the offset to the column label row
+        for x in range(0, (self.size * 2) + 1):
+            res += '{x:>3}'.format(x=x)
+        res += '\n' * 2
+
+        # Add the rest of the grid to the result string
+        label = 0
+        for row in range(0, (self.size*2)+1):
+            # Add the row labels to the string
+            res += '{row_num:<4}'.format(row_num=row)
+            for col in range(0, (self.size*2)+1):
+                if col % 2 == 0 and row % 2 == 1 and (col, row) not in lines:
+                    res += '{:6}'.format('')
+                if (col, row) in lines:
+                    if col % 2 == 0 and row % 2 == 1:
+                        res += '{:6}'.format('|')
+                    elif col % 2 == 1 and row % 2 == 0:
+                        res += '-----'
+                elif (col+1, row) in lines and row % 2 == 0:
+                    res += '*'
+                elif col % 2 == 0 and row % 2 == 0:
+                    res += '{:6}'.format('*')
+            res += '\n' * 2
+        return res
+
+    
+    def update(self, state):
+        """Update the board with a new game state
+        
+        Parameters:
+            state (dict): The new game state object
+        
+        """
+    
+        self.state = state
---
+++ src/utils.py
@@ -0,0 +1,200 @@
+import sys
+from copy import deepcopy
+
+
+class Utils:
+    """Library class with useful functions for the project"""
+
+    def is_completion(state, move):
+        """Checks if a move makes a completion and counts how many completions are made
+
+        Parameters:
+            state (dict): The game state object
+            move (tuple): A tuple of two integers that represents the (x,y) of a move
+
+        Return:
+            completions (int): The number of completions made by the given move in the given state
+
+        """
+        completions = 0
+        lines = set(state[1]) | set(state[2])
+        (x, y) = move
+
+        # Horizontal Line
+        if x % 2 == 1:
+            # Bottom completion
+            if (
+                (x - 1, y - 1) in lines
+                and (x + 1, y - 1) in lines
+                and (x, y - 2) in lines
+            ):
+                completions += 1
+            # Top completion
+            if (
+                (x - 1, y + 1) in lines
+                and (x + 1, y + 1) in lines
+                and (x, y + 2) in lines
+            ):
+                completions += 1
+
+        # Vertical Line
+        if x % 2 == 0:
+            # Left-side completion
+            if (
+                (x + 1, y - 1) in lines
+                and (x + 1, y + 1) in lines
+                and (x + 2, y) in lines
+            ):
+                completions += 1
+            # Right-side completion
+            if (
+                (x - 1, y - 1) in lines
+                and (x - 1, y + 1) in lines
+                and (x - 2, y) in lines
+            ):
+                completions += 1
+
+        return completions
+
+    def make_move(state, move, player, ai_prev_score, player_prev_score):
+        """Add a move to the game state and increment score as needed
+
+        Parameters:
+            state (dict): The current game state object
+            move (tuple): A tuple of two integers that represents the (x,y) of a move
+            player (Player | AI): The player object or AI object who is making the move
+            ai_prev_score (int): The AI player's score before the move occurs
+            player_prev_score (int): The human player's score before the move occurs
+
+        Return:
+            result (dict): The new game state object after the move has been added,
+            completions (int): The number of completions that were made as a result of the move
+
+        """
+        result = deepcopy(state)
+        completions = Utils.is_completion(state, move)
+        result[result["player"]].append(move)
+
+        if player.get_nick() == "AI":
+            total = ai_prev_score + completions
+            player.set_score(max(total, player.get_score()))
+        else:
+            total = player_prev_score + completions
+            player.set_score(max(total, player.get_score()))
+
+        # player.set_score(player.get_score() + completions)
+        return result, completions
+
+    def terminal_test(state, moves):
+        """Determine whether or not there are any valid moves left on the board
+
+        Parameters:
+            state (dict): The current game state object
+            moves (list): All of the valid moves left on the board
+
+        Return:
+            True if no valid moves left, otherwise False
+
+        """
+        return len(moves) == 0
+
+    def valid_moves(state):
+        """Generate all of the valid moves given a game state
+
+        Parameters:
+            state (dict): The current game state object
+
+        Return:
+            result (list): A list of tuples where each tuple is a valid move (x,y)
+
+        """
+        result = []
+        [x_dim, y_dim] = state["board_size"]
+
+        # Create a list of all possible moves regardless of the state
+        for i in range(0, (x_dim * 2) + 1):
+            for j in range(0, (y_dim * 2) + 1):
+                if (i % 2 == 0 and j % 2 == 1) or i % 2 == 1 and j % 2 == 0:
+                    result.append((i, j))
+
+        # Remove any moves that are already taken in the game state
+        result = [
+            m
+            for m in result
+            if (m not in state[1]) and (m not in state[2]) and m != ("0", "0")
+        ]
+        return result
+
+    def read_state_file(file):
+        """Read a file and parse the game state information contained within
+
+        Parameters:
+            file (str): The name of the file that contains game state information
+
+        Return:
+            data (dict): The game state that was represented in the given file
+
+        """
+        try:  # Attempt to open the game state file
+            f = open(file)
+        except Exception as err:
+            print(err)
+            print(f"Error opening game-state file: {file}")
+            sys.exit(0)
+
+        data = {}
+
+        # Read through the file and parse the information
+        for line in f.readlines():
+            line = line.strip()
+
+            # Add the board dimensions to the state - [x_dim, y_dim]
+            if "B=" in line:
+                data["board_size"] = list(
+                    map(lambda x: int(x), line.split("=")[1].split("x"))
+                )
+
+            ####### TODO: Make a function that processes this data;; pass the player as an argument #############
+
+            # Add player one's current board positions
+            if "p1:" in line:
+                positions = line.split("p1: ")
+                if len(positions) == 2:
+                    data[1] = list(
+                        map(
+                            lambda x: tuple(
+                                x.replace("(", "").replace(")", "").split(",")
+                            ),
+                            positions[1].split(" "),
+                        )
+                    )
+                    data[1] = list(map(lambda x: (int(x[0]), int(x[1])), data[1]))
+                else:
+                    data[1] = []
+
+            # Add player two's current board positions
+            if "p2:" in line:
+                positions = line.split("p2: ")
+                if len(positions) == 2:
+                    data[2] = list(
+                        map(
+                            lambda x: tuple(
+                                x.replace("(", "").replace(")", "").split(",")
+                            ),
+                            positions[1].split(" "),
+                        )
+                    )
+                    data[2] = list(map(lambda x: (int(x[0]), int(x[1])), data[2]))
+                else:
+                    data[2] = []
+            ########################################################################## end TODO
+
+        f.close()
+
+        # Add the current player's turn
+        if len(data[1]) == len(data[2]):
+            data["player"] = 1
+        else:
+            data["player"] = 2
+
+        return data
